ea1844b092096774d90cc33bcfed6c878e1094a9
Dan Esparza <esparza.dan@gmail.com>
2019-05-06 13:02:13 -0400

    Update darksky.go
    Return darksky data in common format

--- data/darksky.go
+++ data/darksky.go
@@ -33,6 +33,56 @@ func (s DarksyService) GetWeatherReport(ctx context.Context, lat, long string) (
 		return retval, fmt.Errorf("Error getting forecast: %v", err)
 	}
 
+	//	Get the daily points:
+	dailyPoints := []WeatherDataPoint{}
+	for _, item := range response.Daily.Data {
+
+		//	Grab the current daily point
+		dailyPoint := WeatherDataPoint{
+			ApparentTemperature: item.ApparentTemperature,
+			CloudCover:          item.CloudCover,
+			Humidity:            item.Humidity,
+			Icon:                item.Icon,
+			Ozone:               item.Ozone,
+			PrecipAccumulation:  item.PrecipAccumulation,
+			PrecipIntensity:     item.PrecipIntensity,
+			PrecipIntensityMax:  item.PrecipIntensityMax,
+			PrecipProbability:   item.PrecipProbability,
+			PrecipType:          item.PrecipType,
+			Pressure:            item.Pressure,
+			Summary:             item.Summary,
+			Temperature:         item.Temperature,
+			TemperatureMax:      item.TemperatureMax,
+			Time:                item.Time,
+			Visibility:          item.Visibility,
+			WindBearing:         item.WindBearing,
+			WindGust:            item.WindGust,
+			WindSpeed:           item.WindSpeed,
+		}
+
+		//	Add our daily point:
+		dailyPoints = append(dailyPoints, dailyPoint)
+	}
+
+	//	Get the alerts
+	alerts := []WeatherAlert{}
+	for _, item := range response.Alerts {
+
+		//	Grab the current alert:
+		alert := WeatherAlert{
+			Title:       item.Title,
+			Regions:     item.Regions,
+			Severity:    item.Severity,
+			Description: item.Description,
+			Time:        item.Time,
+			Expires:     item.Expires,
+			URI:         item.URI,
+		}
+
+		//	Add the alert:
+		alerts = append(alerts, alert)
+	}
+
 	//	Format our weather report
 	retval = WeatherReport{
 		Latitude:  response.Latitude,
@@ -58,6 +108,12 @@ func (s DarksyService) GetWeatherReport(ctx context.Context, lat, long string) (
 			WindGust:            response.Currently.WindGust,
 			WindSpeed:           response.Currently.WindSpeed,
 		},
+		Daily: WeatherDataBlock{
+			Summary: response.Daily.Summary,
+			Icon:    response.Daily.Icon,
+			Data:    dailyPoints,
+		},
+		Alerts:   alerts,
 		APICalls: response.APICalls,
 		Code:     response.Code,
 	}
