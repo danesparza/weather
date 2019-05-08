cac526d6f6a9ed24464d78ccaa54bb67fca30eb8
Dan Esparza <danesparza@cagedtornado.com>
2019-05-08 11:00:42 -0400

    Update main.go
    Updated segment name

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ type Message struct {
 // HandleRequest handles the AWS lambda request
 func HandleRequest(ctx context.Context, msg Message) (data.WeatherReport, error) {
 	xray.Configure(xray.Config{LogLevel: "trace"})
-	ctx, seg := xray.BeginSegment(ctx, "pollen-lambda-handler")
+	ctx, seg := xray.BeginSegment(ctx, "weather-lambda-handler")
 
 	//	Set the services to call with
 	services := []data.WeatherService{
